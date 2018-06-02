1431d10a5cd9de42a49478dbbaf4b5943bbf8bfe
caixian <qucaixian@qq.com>
2018-06-02 19:45:42 +0800

    按需加载不管了

--- src/App.js
+++ src/App.js
@@ -1,28 +1,5 @@
-// import React, { Component } from 'react';
-// import logo from './logo.svg';
-// import './App.css';
-// import {createStore} from 'redux';
-//
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-//       </div>
-//     );
-//   }
-// }
-//
-// export default App;
-
 import React from 'react';
-import {Button,List} from 'antd-mobile';
+import {Button, List} from 'antd-mobile';
 import 'antd-mobile/dist/antd-mobile.css';
 
 class App extends React.Component {
@@ -48,36 +25,33 @@ class Children extends React.Component {
         this.state = {
             solders: ['马飞', '卢本伟']
         };
-        // this.addSolder = this.addSolder().bind(this);
     }
-    componentWillMount()
-    {
+
+    componentWillMount() {
         console.log('组件prepare')
     }
-    componentDidMount()
-    {
+
+    componentDidMount() {
         console.log('组件ok')
     }
 
-    addSolder(){
+    addSolder() {
         console.log('卢本伟牛逼！');
         this.setState({
-            solders:[...this.state.solders,'观众老爷'+Math.random()]
+            solders: [...this.state.solders, '观众老爷' + Math.random()]
         })
-        // console.log([...this.state.solders])
     }
 
     render() {
         console.log('组件ing');
-        // const boss = 'zhang';
         return (
             <div>
                 <h2>{this.props.boss}，牛逼！</h2>
-                <Button type='primary' onClick={()=>this.addSolder()}>加入直播间</Button>
+                <Button type='primary' onClick={() => this.addSolder()}>加入直播间</Button>
                 <List
-                    renderHeader={()=>'主播们'}
+                    renderHeader={() => '主播们'}
                 >
-                    {this.state.solders.map(v=>{
+                    {this.state.solders.map(v => {
                         return (
                             <List.Item key={v}>
                                 {v}
@@ -85,11 +59,11 @@ class Children extends React.Component {
                         )
                     })}
                 </List>
-                <ul>
-                    {this.state.solders.map(v => {
-                        return <li key={v}>{v}</li>
-                    })}
-                </ul>
+                {/*<ul>*/}
+                    {/*{this.state.solders.map(v => {*/}
+                        {/*return <li key={v}>{v}</li>*/}
+                    {/*})}*/}
+                {/*</ul>*/}
             </div>
         )
     }
