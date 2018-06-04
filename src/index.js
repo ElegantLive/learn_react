edccbb2cc1209de8e4f6e0d29142655192f0f1d9
caixian <qucaixian@qq.com>
2018-06-04 16:15:50 +0800

    改写connct

--- src/index.js
+++ src/index.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import {createStore,applyMiddleware,compose} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
 import App from './App';
-import { counter,add,unfollow,addAsync} from "./index.redux";
+import {counter} from "./index.redux";
 
 const reduxDectools = window.devToolsExtension;
 
-const store = createStore(counter,compose(
+const store = createStore(counter, compose(
     applyMiddleware(thunk),
-    reduxDectools?reduxDectools():f=>f
+    reduxDectools ? reduxDectools() : f => f
 ));
-function render() {
-    ReactDom.render(<App store={store} add={add} unfollow={unfollow} addAsync={addAsync}/>,document.getElementById('root'));
-    console.log(store.getState())
-}
 
-render();
-
-store.subscribe(render);
+ReactDom.render(
+    (<Provider store={store}>
+        <App/>
+    </Provider>), document.getElementById('root'));
